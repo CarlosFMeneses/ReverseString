782b651e2edf0372f57719c3569be03822be307b
Carlos F. Meneses <CarlosFMeneses@users.noreply.github.com>
2019-03-29 00:05:20 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Reverse String
+[![codebeat badge](https://codebeat.co/badges/2c76e576-1805-479b-8b2a-8cfa373b0df8)](https://codebeat.co/projects/github-com-carlosfmeneses-classroomcontroller-master)
+Enter a string and the program will reverse it and print it out.
+
+_from [Martyr2'S Mega Project Ideas List!](https://www.dreamincode.net/forums/topic/78802-martyr2s-mega-project-ideas-list "Martyr2'S Mega Project Ideas List!")_
---
+++ src/ReverseString.java
@@ -0,0 +1,24 @@
+/**
+ * ReverseString//ReverseString.java
+ * @author carlosfmeneses
+ * Created on 36/28/2019
+ * Modified on 36/28/2019
+ */
+
+/**
+ *  ReverseString 
+ *  Enter a string and the program will reverse it and print it out
+ *  from Martyr2'S Mega Project Ideas List!
+ *  https://www.dreamincode.net/forums/topic/78802-martyr2s-mega-project-ideas-list/
+ */
+public class ReverseString {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("HelloWorld!");
+
+	}
+
+}
